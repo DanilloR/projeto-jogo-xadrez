a06156f2e0d32a4a9476e90b80aa065ead2411a4
DanilloR <danillo.ramos@dcomp.ufs.br>
2020-06-01 12:08:30 -0300

    Implementação do tabuleiro, peças e posições

---
+++ src/application/JogoXadrez.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Tabuleiro;
+
+public class JogoXadrez {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Tabuleiro tabuleiro = new Tabuleiro(8,8);
+
+	}
+
+}
